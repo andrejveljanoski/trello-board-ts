43913fdb7b6e50231a4ad87e11da032be5a0db9d
andrejveljanoski <andrejveljanoskif1@gmail.com>
2024-05-16 18:23:30 +0200

    initial project setup

---
+++ webpack.config.ts
@@ -0,0 +1,31 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+};
